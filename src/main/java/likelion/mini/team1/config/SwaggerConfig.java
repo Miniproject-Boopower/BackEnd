1774e5baa33647efc0cea2e02bc049cd51896735
HyunWoo9930 <ohw9930@hufs.ac.kr>
2025-05-12 16:10:21 +0900

    Feat : swagger url 추가

--- src/main/java/likelion/mini/team1/config/SwaggerConfig.java
+++ src/main/java/likelion/mini/team1/config/SwaggerConfig.java
@@ -1,10 +1,14 @@
 package likelion.mini.team1.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
@@ -16,8 +20,15 @@ public class SwaggerConfig {
 			.version("1.0")
 			.description("API for managing users and their profile images.");
 
+		Server server1 = new Server().url("http://localhost:8080");
+		Server server2 = new Server().url("https://mini1team.lion.it.kr");
+		List<Server> servers = new ArrayList<>();
+		servers.add(server1);
+		servers.add(server2);
+
 		return new OpenAPI()
-			.info(info);
+			.info(info)
+			.servers(servers);
 		// .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
 		// .components(new io.swagger.v3.oas.models.Components());
 		// .addSecuritySchemes("Bearer Authentication", new SecurityScheme()
