cc5538e82d6108482e6c5c9382e05feb366aa962
HyunWoo9930 <ohw9930@hufs.ac.kr>
2025-05-27 23:09:20 +0900

    Feat : deletemapping import 추가

--- src/main/java/likelion/mini/team1/controller/UserController.java
+++ src/main/java/likelion/mini/team1/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -110,10 +111,6 @@ public class UserController {
 		userService.deleteActivity1(studentNumber, activityId);
 		return ResponseEntity.ok("1학기 활동이 삭제되었습니다.");
 	}
-
-
-
-
 
 	@GetMapping("/main/today/assignment")
 	public ResponseEntity<?> getTodayAssignment(@RequestParam String studentNumber) {
